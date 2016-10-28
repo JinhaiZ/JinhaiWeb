a5e5c366eef1a8f6e8abcad5455b2f1844f8afc6
Jinhai ZHOU <zhoujinhaixd@gmail.com>
2016-10-28 13:41:28 +0200

    refactorying original code

---
+++ assets/js/myWeb/myWeb.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('myWeb', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            // route for the home page
+            .state('home', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/navbar/home.navbar.view.html',
+                    },
+                    'content': {
+                        templateUrl : 'views/home.view.html',
+                        // controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.view.html',
+                    }
+                }
+
+            })
+
+            // route for the aboutus page
+            .state('contact', {
+                url:'/contact',
+                views: {
+                    'header': {
+                        templateUrl : 'views/navbar/contact.navbar.view.html',
+                    },
+                    'contact': {
+                        templateUrl : 'views/contact.view.html',
+                        // controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.view.html',             
+                    }
+                }
+            })
+        $urlRouterProvider.otherwise('/');
+    });
